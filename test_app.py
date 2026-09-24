e2aefe46a38dd95fbd7a6e669c3d082f497d1b75
harshmishra170206 <mishraharsh1702@gmail.com>
2026-09-24 10:28:47 +0530

    Implement unit tests for app functions
    Added unit tests for square and is_even functions.

--- test_app.py
+++ test_app.py
@@ -1,1 +1,9 @@
-'''writing the test functions for app.py
+import unittest
+from app import square, is_even
+
+class TestApp(unittest.TestCase):
+    def test_square(self):
+        self.assertEqual(square(4), 16)
+
+    def test_is_even(self):
+        self.assertTrue(is_even(4))
